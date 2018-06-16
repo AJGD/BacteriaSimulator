31ad37a4ea709d698d6d3a8c697b4d285aef9a6a
Wojciech Basiura <basiura.wojciech@gmail.com>
2018-06-16 21:54:24 +0200

    Avarage values are now doubles.

--- src/main/scala/StatisticsGen.scala
+++ src/main/scala/StatisticsGen.scala
@@ -17,11 +17,11 @@ class StatisticsGen() extends Actor {
          println(s"After antibiotic ${report.deaths} bacteria died.")
       }
       println(s"\n\n###On avarage:\n\n" +
-              s"${data.foldLeft(0)((retVal: Int, report: Report) => retVal + report.survivals) / data.size} was bacteria max population.\n\n" +
-              s" > ${data.foldLeft(0)((retVal: Int, report: Report) => retVal + report.photosynthesis) / data.size} evolved to photosynthesis.\n" +
-              s" > ${data.foldLeft(0)((retVal: Int, report: Report) => retVal + report.sterilization) / data.size} evolved to sterilization.\n" +
-              s" > ${data.foldLeft(0)((retVal: Int, report: Report) => retVal + report.preservationOverFertility) / data.size} evolved to preservationOverFertility.\n")
-      println(s"After antibiotic ${data.foldLeft(0)((retVal: Int, report: Report) => retVal + report.deaths) / data.size} bacteria died")
+              s"${data.foldLeft(0)((retVal: Int, report: Report) => retVal + report.survivals).toDouble / data.size} was bacteria max population.\n\n" +
+              s" > ${data.foldLeft(0)((retVal: Int, report: Report) => retVal + report.photosynthesis).toDouble / data.size} evolved to photosynthesis.\n" +
+              s" > ${data.foldLeft(0)((retVal: Int, report: Report) => retVal + report.sterilization).toDouble / data.size} evolved to sterilization.\n" +
+              s" > ${data.foldLeft(0)((retVal: Int, report: Report) => retVal + report.preservationOverFertility).toDouble / data.size} evolved to preservationOverFertility.\n")
+      println(s"After antibiotic ${data.foldLeft(0)((retVal: Int, report: Report) => retVal + report.deaths).toDouble / data.size} bacteria died")
     }
     case _ => println("The heck?")
   }
