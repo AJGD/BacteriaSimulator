a31b1e559cae47fa6cb57f98fd0e0588592ad473
AJGD <you@example.com>
2018-06-18 20:52:36 +0200

    minor test change

--- src/test/scala/RaportSpec.scala
+++ src/test/scala/RaportSpec.scala
@@ -6,7 +6,7 @@ import main.scala.{ Report, Photosynthesis, Sterilization, PreservationOverFerti
 
 class ReportSpec() extends FlatSpec with Matchers {
     "A Raport" should "create with one or increment by one value assigned to mutation" in {
-        val report = new Report(0, 0, collection.mutable.Map())
+        val report = new Report()
         for(i <- 1 to 10) {
             report.oneMore(Photosynthesis())
             report.oneMore(Sterilization())
@@ -21,7 +21,7 @@ class ReportSpec() extends FlatSpec with Matchers {
     }
 
     it should "throw NoSuchElementException if an element isn't found" in {
-        val emptyReport = new Report(0, 0, collection.mutable.Map())
+        val emptyReport = new Report()
         a [NoSuchElementException] should be thrownBy {
             emptyReport.map(Photosynthesis())
             emptyReport.map(Sterilization())
