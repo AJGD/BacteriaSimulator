0004523a17bcaaba42cc4db8d5d273b8e18a9ee2
Wojciech Basiura <basiura.wojciech@gmail.com>
2018-06-19 10:28:32 +0200

    Output format small changes.

--- src/main/scala/Report.scala
+++ src/main/scala/Report.scala
@@ -13,6 +13,6 @@ class Report(var survivals: Integer = 0, var deaths: Integer = -1,
     def print() = {
         println(s"\n${survivals} was bacteria max population.\n")
         map.foreach {case (key, value) => println(s" > ${value} evolved to ${key.name()}.")}
-        println(s"After antibiotic ${deaths} bacteria died.\n")
+        println(s"\nAfter antibiotic ${deaths} bacteria died.\n")
     }
 }
